56e94e6b7609c163145828cfcdff816ed6c8b156
Matthew Buckton <matthew@buckton.org>
2024-04-10 21:24:23 +1000

    move to  devias-io as the basis of the dashboard.

--- orval.config.js
+++ orval.config.js
@@ -1,22 +1,22 @@
- module.exports = {
-     'maps-messaging-file-transfomer': {
-         output: {
-             mode: 'tags-split',
-             target: './src/generated/mapsServer.ts',
-             schemas: './src/generated/model',
-             client: 'react-query',
-             mock: false,
-             override: {
-                 query: {
-                     useQuery: true,
-                     options: {
-                         staleTime: 30000,
-                     },
-                 },
-             },
-         },
-         input: {
-             target: './resources/openapi.json',
-         },
-     },
- };
+module.exports = {
+    'dojomanagement-file-transfomer': {
+        output: {
+            mode: 'tags-split',
+            target: './src/generated/mapsmessaging.ts',
+            schemas: './src/generated/model',
+            client: 'react-query',
+            mock: false,
+            override: {
+                query: {
+                    useQuery: true,
+                    options: {
+                        staleTime: 5000,
+                    },
+                },
+            },
+        },
+        input: {
+            target: './resources/api-docs.json',
+        },
+    },
+};
