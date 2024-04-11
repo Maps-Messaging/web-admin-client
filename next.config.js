8090eb1495310ddcf3d63f27648336146c314f6c
Matthew Buckton <matthew@buckton.org>
2024-04-11 17:17:01 +1000

    First pass at moving the app to /admin on the uri

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@
  */
 const nextConfig = {
   output: 'export',
+  basePath: '/admin',
+  assetPrefix: '/admin'
 
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
