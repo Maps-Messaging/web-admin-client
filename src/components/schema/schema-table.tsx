cd534a9405e5cce966ecc5c0b97669ded030098d
Matthew Buckton <matthew@buckton.org>
2024-04-18 11:05:15 +1000

    need this file as well

---
+++ src/components/schema/schema-table.tsx
@@ -0,0 +1,71 @@
+'use client';
+
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import Divider from '@mui/material/Divider';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+
+import {type SchemaConfig} from "@/generated/model";
+import {SchemaRow} from "@/components/schema/schema-row";
+
+function noop(): void {
+  // do nothing
+}
+
+interface SchemaTableProps {
+  count?: number;
+  page?: number;
+  rows?: SchemaConfig[];
+  rowsPerPage?: number;
+}
+
+export function SchemaTable({
+                                 count = 0,
+                                 rows = [],
+                                 page = 0,
+                                 rowsPerPage = 0,
+                               }: SchemaTableProps): React.JSX.Element {
+  return (
+    <Card>
+      <Box sx={{ overflowX: 'auto' }}>
+        <Table sx={{ minWidth: '800px' }}>
+          <TableHead>
+            <TableRow>
+              <TableCell>Format</TableCell>
+              <TableCell>Type</TableCell>
+              <TableCell>Unique Id</TableCell>
+              <TableCell>Created</TableCell>
+              <TableCell>Version</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => {
+              return (
+                <SchemaRow
+                  key={row?.uniqueId || ''}
+                  schema={row}
+                />
+              );
+            })}
+          </TableBody>
+        </Table>
+      </Box>
+      <Divider />
+      <TablePagination
+        component="div"
+        count={count}
+        onPageChange={noop}
+        onRowsPerPageChange={noop}
+        page={page}
+        rowsPerPage={rowsPerPage}
+        rowsPerPageOptions={[5, 10, 25]}
+      />
+    </Card>
+  );
+}
