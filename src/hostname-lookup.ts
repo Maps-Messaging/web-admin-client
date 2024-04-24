a232a0403a9ef29b99245e6dc074d0b1c5e040d5
Matthew Buckton <matthew@buckton.org>
2024-04-24 17:58:36 +1000

    cleanup

--- src/hostname-lookup.ts
+++ src/hostname-lookup.ts
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 axios.defaults.baseURL = process.env.API_BASE_URL;
 
-
 axios.interceptors.request.use(req => {
   return req;
 });
--- src/paths.ts
+++ src/paths.ts
@@ -4,15 +4,14 @@ export const paths = {
   auth: { signIn: '/auth/sign-in', signUp: '/auth/sign-up', resetPassword: '/auth/reset-password' },
   dashboard: {
     overview: '/dashboard',
-    monitor: '/dashboard/monitor',
     users: '/dashboard/authentication/users',
     groups: '/dashboard/authentication/groups',
+    monitor: '/dashboard/monitor',
     namespace: '/dashboard/namespace',
     network: '/dashboard/network',
     connections: '/dashboard/connections',
     integrations: '/dashboard/integrations',
     schemas: '/dashboard/schema',
     settings: '/dashboard/settings',
-  },
-  errors: { notFound: '/errors/not-found' },
+  }
 } as const;
